219e5031d22ecf2527d18f79303a9dfa0087a341
rafael.araujo <araujrafa@gmail.com>
2017-05-03 16:38:18 -0300

    alterando acessos do crud, acrescentando comando no leia-me e ignorando o acessos.js

--- model/crudInsumo.js
+++ model/crudInsumo.js
@@ -1,15 +1,7 @@
 var pg = require('pg');
 var path = require('path');
-
-var config = {
-  user: 'postgres', //env var: PGUSER 
-  database: 'gestaofazenda', //env var: PGDATABASE 
-  password: '020205', //env var: PGPASSWORD 
-  host: 'localhost', // Server hosting the postgres database 
-  port: 5432, //env var: PGPORT 
-  max: 10, // max number of clients in the pool 
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
-};
+const acessos = require('../database/acessos.js')
+var config = acessos.config;
 
 module.exports = {
     insert : function(arrayData) {
