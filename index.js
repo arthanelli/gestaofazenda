ad874a2ded183a1c57a4f517722b4a07042a7b97
Rafael Araujo <araujrafa@gmail.com>
2017-05-13 14:10:28 -0300

    adicionando arquivos faltantes

---
+++ index.js
@@ -0,0 +1,216 @@
+var Hapi = require('hapi');
+var Path = require('path');
+var crudInsumo = require('./model/crudInsumo.js')  
+var crudFinancas = require('./model/crudFinancas.js') 
+var crudAnaliseLeite = require('./model/crudAnaliseLeite.js')
+var crudTerreno = require('./model/crudTerreno.js')
+var crudVacinacao = require('./model/crudVacinacao.js')
+
+var Inert = require('inert');
+
+var server = new Hapi.Server({
+	connections: {
+			routes: {
+				files: {
+						relativeTo: Path.join(__dirname, 'views')
+				}
+			}
+    }
+});
+
+server.connection({
+	host: 'localhost',
+	port: 3000
+});
+
+
+server.register([require('vision'), Inert], (err) => {
+
+	if (err) {
+			throw err;
+	}
+
+	server.route({  
+		method: 'GET',
+		path: '/css/{file*}',
+		handler: {
+			directory: { 
+				path: './css/'
+			}
+		}
+	});
+
+	server.route({  
+		method: 'GET',
+		path: '/js/{file*}',
+		handler: {
+			directory: { 
+				path: './js/'
+			}
+		}
+	});
+
+	server.views({  
+		engines: {
+				html: require('handlebars')
+		},
+		path: 'views',
+		layoutPath: 'views/layout',
+		layout: 'default',
+		partialsPath: 'views/partials'
+		//helpersPath: 'views/helpers',
+	});
+
+	
+	server.route({
+		method: 'GET',
+			path: '/',
+			handler: function(request, reply) {
+
+				var data = {
+						title: 'Home',
+				};
+
+				return reply.view('index', data);
+			}
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/insert',
+		config: {
+			payload: {
+				output: 'data',
+				parse: true
+			},
+			handler: function(request, reply){
+			var urlRequest = request.info.referrer.split('/');
+			var fileRequest = urlRequest[urlRequest.length - 1];		
+
+			if(fileRequest == 'incluirAnaliseLeite.html') {
+				crudAnaliseLeite.insert(request.payload);
+			} else if (fileRequest == 'incluirFinancas.html') {
+				crudFinancas.insert(request.payload);
+			} else if (fileRequest == 'incluirInsumos.html') {
+				crudInsumo.insert(request.payload);
+			} else if (fileRequest == 'incluirTerreno.html') {
+				crudTerreno.insert(request.payload);
+			}
+			}
+		}
+	});
+
+    server.route({
+    	method: 'GET',
+    	path: '/read',
+    	config: {
+    		handler: function(request, reply){
+    			var resultados = [];
+				var nameCrud = request.payload.nameCrud;	
+
+				if(nameCrud == 'analiseLeite') {
+					crudAnaliseLeite.read(function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'financas') {
+					crudFinancas.read(function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'insumos.html') {
+					crudInsumo.read(function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'terreno.html') {
+					crudTerreno.read(function(vetor){
+						resultados = vetor;
+					});
+				}	
+    		}
+    	}
+    });
+
+	server.route({
+    	method: 'GET',
+    	path: '/returnChange',
+    	config: {
+    		handler: function(request, reply){
+    			var resultados = [];
+				var nameCrud = request.payload.nameCrud;	
+
+				if(nameCrud == 'analiseLeite') {
+					crudAnaliseLeite.returnChange(request.payload, function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'financas') {
+					crudFinancas.returnChange(request.payload, function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'insumos.html') {
+					crudInsumo.returnChange(request.payload, function(vetor){
+						resultados = vetor;
+					});
+				} else if (nameCrud == 'terreno.html') {
+					crudTerreno.returnChange(request.payload, function(vetor){
+						resultados = vetor;
+					});
+				}
+    		}
+    	}
+    });
+
+	server.route({
+    	method: 'POST',
+    	path: '/change',
+    	config: {
+    		payload: {
+    			output: 'data',
+    			parse: true
+    		},
+    		handler: function(request, reply){
+				var nameCrud = request.payload.nameCrud;	
+
+				if(nameCrud == 'analiseLeite') {
+					crudAnaliseLeite.change(request.payload);
+				} else if (nameCrud == 'financas') {
+					crudFinancas.change(request.payload);
+				} else if (nameCrud == 'insumos.html') {
+					crudInsumo.change(request.payload);
+				} else if (nameCrud == 'terreno.html') {
+					crudTerreno.change(request.payload);
+				}
+    		}
+    	}
+	});
+
+    server.route({
+    	method: 'POST',
+    	path: '/deletar',
+    	config: {
+    		payload: {
+    			output: 'data',
+    			parse: true
+    		},
+    		handler: function(request, reply){
+				var nameCrud = request.payload.nameCrud;	
+
+				if(nameCrud == 'analiseLeite') {
+					crudAnaliseLeite.del(request.payload);
+				} else if (nameCrud == 'financas') {
+					crudFinancas.del(request.payload);
+				} else if (nameCrud == 'insumos.html') {
+					crudInsumo.del(request.payload);
+				} else if (nameCrud == 'terreno.html') {
+					crudTerreno.del(request.payload);
+				}
+    		}
+    	}
+	});
+
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
---
+++ views/js/main.js
@@ -0,0 +1,14 @@
+
+window.onload = function(){
+	var openConfig = false;
+
+	$('.logoff').on('click', function(){
+		if(openConfig == true) {
+			$('.box-logoff').css('display', 'none');
+			openConfig = false;
+		} else if (openConfig == false) {
+			$('.box-logoff').css('display', 'block');
+			openConfig = true;
+		}
+	})
+}
