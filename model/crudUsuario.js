aba2391c9836f36e003768eff026e0fc5afebe33
Igor Apolinário <igor_apolinario96@hotmail.com>
2017-09-09 00:36:55 -0300

    Inicio da implantação do gerenciamento de usuários

---
+++ model/crudUsuario.js
@@ -0,0 +1,179 @@
+var pg = require('pg');
+var path = require('path');
+const acessos = require('../database/acessos.js')
+var config = acessos.config;
+
+module.exports = {
+    insert : function(arrayData) {
+        var results = [];
+        // Grab data from http request
+        var data = {
+        	codUsuario: arrayData.codUsuario,
+            nome: arrayData.nome,
+        	idade: arrayData.idade, 
+            sexo: arrayData.sexo,
+            dataNascimento: arrayData.dataNascimento,
+        	endereco: arrayData.endereco, 
+            nivelPermissao: arrayData.nivelPermissao
+        };
+        // Get a Postgres client from the connection pool
+        pg.connect(config, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                //return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Insert Data
+            client.query('INSERT INTO usuarios(codUsuario , nome, idade, sexo, dataNascimento, endereco, nivelPermissao) values($1, $2, $3, $4, $5, $6, $7)',
+            [data.codUsuario , data.nome, data.idade, data.sexo, data.dataNascimento, data.endereco, data.nivelPermissao]);
+            // SQL Query > Select Data
+            var query = client.query('SELECT * FROM insumos ORDER BY id ASC');
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+                done();
+                //console.log(results);
+                //return ;
+                // if (callback != null) {
+                //     callback(results);
+                // }   
+            });
+        });
+    },
+    read : function(callback) {
+        var results = [];
+        // Get a Postgres client from the connection pool
+        pg.connect(config, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                //return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Select Data
+            var query = client.query('SELECT * FROM usuarios ORDER BY codUsuario ASC;');
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+                done();
+                //console.log(results);
+                if(callback != null) {
+                    callback(results);
+                }
+            });
+        });
+    },
+    returnChange : function(codUsuario, callback) {
+        var results = [];
+        data = {
+            codUsuario : codUsuario
+        }
+        // Get a Postgres client from the connection pool
+        pg.connect(config, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                //return res.status(500).json({success: false, data: err});
+            }
+            
+            // SQL Query > Select Data
+            var query = client.query('SELECT * FROM usuarios WHERE codusuario = $1', [data.codUsuario]);
+            
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+                done();
+                //console.log(results);
+                if(callback != null) {
+                    callback(results);
+                }
+            });
+        });
+    },
+    change : function(arrayData, callback) {
+        var results = [];
+
+        // Grab data from the URL parameters
+        var codUsuario = arrayData.codUsuario;
+        var erro = false;
+
+        // Grab data from the URL parameters
+        var data = {
+        	codUsuario: arrayData.codUsuario,
+            nome: arrayData.nome,
+        	idade: arrayData.idade,
+            sexo: arrayData.sexo,
+            dataNascimento: arrayData.dataNascimento,
+        	endereco: arrayData.endereco, 
+            nivelPermissao: arrayData.nivelPermissao
+        };
+        // Get a Postgres client from the connection pool
+        pg.connect(config, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                //return res.status(500).json({success: false, data: err});
+                if(callback != null) {
+                    callback(erro);
+                }
+            }
+            // SQL Query > Update Data
+            client.query('UPDATE usuarios SET nome=($1), idade=($2), sexo=($3), datanascimento=($4), endereco=($5), nivelPermissao=($6) WHERE codUsuario=($7)',
+            [data.nome, data.idade, data.sexo, data.dataNascimento, data.endereco, data.nivelPermissao, codUsuario]);
+            // SQL Query > Select Data
+            var query = client.query("SELECT * FROM usuarios ORDER BY codUsuario ASC");
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', function() {
+                done();
+
+                if(callback != null) {
+                    callback(erro);
+                }
+                //return res.json(results);
+            });
+        });
+    },
+    del : function(codUsuario) {
+        var results = [];
+        // Grab data from the URL parameters
+        // Get a Postgres client from the connection pool
+        pg.connect(config, (err, client, done) => {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+               // return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Delete Data
+            client.query('DELETE FROM usuarios WHERE codUsuario=($1)', [codUsuario]);
+            // SQL Query > Select Data
+            var query = client.query('SELECT * FROM usuarios ORDER BY codUsuario ASC');
+            // Stream results back one row at a time
+            query.on('row', (row) => {
+                results.push(row);
+            });
+            // After all data is returned, close connection and return results
+            query.on('end', () => {
+                done();
+                //return res.json(results);
+            });
+        });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var vacinas = require('./vacinasRotas');
 var insumos = require('./insumosRotas');
 var financas = require('./financeiroRotas');
 var gado = require('./gadoRotas');
+var usuarios = require('./usuariosRotas');
 var index = require('./paginaInicialRotas');
 
-module.exports = [].concat(analisaLeite, vacinas, insumos, financas, gado, index);
+module.exports = [].concat(analisaLeite, vacinas, insumos, financas, gado, usuarios, index);
