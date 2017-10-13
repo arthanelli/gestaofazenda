075404e8f2c7cd03be7b59f7651463db3727282a
Rafael Araujo <araujrafa@gmail.com>
2017-10-12 21:29:10 -0300

    ciclo do gado

--- app/model/ciclo.js
+++ app/model/ciclo.js
@@ -1,25 +1,30 @@
-const gado = [
-  {
-    id: 1,
-    nome: 'mimosa',
-    status: 'gravida',
-    dataNascimento: '10/12/2014',
-    dataCiclo: '10/03/2017',
-  },
-  {
-    id: 2,
-    nome: 'super',
-    status: 'bezzero',
-    dataNascimento: '10/01/2015',
-    dataCiclo: '',
-  }
-]
+const pg = require('pg');
+const path = require('path');
+const acessos = require('../database/acessos.js')
+const config = acessos.config;
+
+// const gado = [
+//   {
+//     id: 1,
+//     nome: 'mimosa',
+//     status: 'gravida',
+//     dataNascimento: '10/12/2014',
+//     dataCiclo: '10/03/2017',
+//   },
+//   {
+//     id: 2,
+//     nome: 'super',
+//     status: 'bezzero',
+//     dataNascimento: '10/01/2015',
+//     dataCiclo: '',
+//   }
+// ]
 
 function setNotification(msg) {
   console.log(msg);
 }
 
-function getToday () {
+function getToday (flag = false) {
   const d = new Date();
   const today = {
     year: d.getFullYear(),
@@ -27,11 +32,13 @@ function getToday () {
     day: d.getDate(),
   }
 
+  if (flag) return `${today.day}/${today.month}/${today.year}`;
+
   return today
 }
 
 function getAge(birth) {
-  if (birth === '') {
+  if (birth === '' || birth === undefined) {
     return '';
   }
 
@@ -55,9 +62,18 @@ function getAge(birth) {
   return age;
 }
 
-function setCiclo(ciclo, id) {
+function setCiclo(ciclo, brinco) {
+  pg.connect(config, (err, client, done) => {
+    if(err) {
+      done();
+      console.log(err);
+    }
+    client.query('UPDATE gado SET status=($1), dataciclo=($2) WHERE brinco=($3)',
+    [ciclo, getToday(true), brinco]);
+  });    
   // INSERT ID CICLO E DATA DO CICLO (TODAY())
-}
+};
+console.log(getToday())
 
 const rules = {
   gravida: 2,
@@ -65,22 +81,35 @@ const rules = {
 
 
 const today = getToday();
+// module.exports = () => {
+//   console.log('oi')
+	pg.connect(config, (err, client, done) => {
+		if(err) {
+      done();
+      console.log(err);
+    }
+    const gado = [];
+		const query = client.query('SELECT * FROM gado ORDER BY brinco ASC;');
+    
+    query.on('row', elem => {
+      const age = getAge(elem.datanascimento);
+		  const dateCycle = getAge(elem.dataCiclo);
 
-gado.forEach(elem => {
-  const age = getAge(elem.dataNascimento);
-  const dateCycle = getAge(elem.dataCiclo);
+		  if (age.year >= rules.gravida && elem.status !== 'PODE-GRAVIDA') {
+		    setCiclo('PODE-GRAVIDA', elem.brinco);
+		    setNotification(`${elem.nome} precisa ficar Grávida`);
+		  } else if (elem.status === 'GRAVIDA' && dateCycle.month > 7) {
+		    setCiclo('PRODUCAO', elem.brinco);
+		    setNotification(`${elem.nome} PODE ENTRAR EM PRODUÇÃO`);
+		  } else if (elem.status === 'PRODUCAO' && dateCycle.month > 1) {
+		    setCiclo('PARTO', elem.brinco);
+		    setNotification(`${elem.nome} Está perto de nascer novo bezzero`);
+		  } else if (elem.status === 'PARTO' && dateCycle.month > 2) {
+		    setCiclo('RECEM-PARTO', elem.brinco);
+        setNot
+      }
+    });
+  });
+  
+// }
 
-  if (age.year >= rules.gravida && elem.status !== 'PODE-GRAVIDA') {
-    setCiclo('PODE-GRAVIDA', elem.id);
-    setNotification(`${elem.nome} precisa ficar Grávida`);
-  } else if (elem.status === 'GRAVIDA' && dateCycle.month > 7) {
-    setCiclo('PRODUCAO', elem.id);
-    setNotification(`${elem.nome} PODE ENTRAR EM PRODUÇÃO`);
-  } else if (elem.status === 'PRODUCAO' && dateCycle.month > 1) {
-    setCiclo('PARTO', elem.id);
-    setNotification(`${elem.nome} Está perto de nascer novo bezzero`);
-  } else if (elem.status === 'PARTO' && dateCycle.month > 2) {
-    setCiclo('RECEM-PARTO', elem.id);
-    setNotification(`${elem.nome} JÁ ESTA PRONTA PARA NOVA GRAVIDEZ`);
-  }
-});
