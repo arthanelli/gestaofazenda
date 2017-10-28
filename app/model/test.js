e78037be042816aab3061fde2b1b4b81474c4860
Rafael Araujo <araujrafa@gmail.com>
2017-10-28 14:30:46 -0200

    adicionando graficos

--- app/model/test.js
+++ app/model/test.js
@@ -1,4 +1,7 @@
-const dieta = require('./dieta.js')
-dieta(1233333333, indicacao => {
-  console.log(indicacao)
-})
+// const dieta = require('./dieta.js')
+// dieta(1233333333, indicacao => {
+  // console.log(indicacao)
+// })
+
+const n = require('./notification');
+n.setNotification('nova');
